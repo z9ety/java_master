e5c30c9ee4d2389b04c867a4fff4f55152327ba0
admin <admin@DESKTOP-IT47SSO>
2023-11-28 10:00:16 +0900

    aaa

---
+++ hgjava/src/chsp6/Member.java
@@ -0,0 +1,10 @@
+package chsp6;
+// 회원관리 앱.
+// 회원번호, 회원이름, 연락처, 성별
+public class Member {
+	// 속성(필드)
+	int memberNo; //자바에서는 두단어이상의 조합: CamelCase.
+	String memberName;
+	String phone; //010-1234-5667
+	String gender;
+}
